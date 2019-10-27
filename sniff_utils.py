474b7835d7f1365c9ba78094240415f835862f69
root <root@localhost.localdomain>
2019-10-27 22:02:45 +0100

    Solved sniffing problem. Started working on packet analysis

--- sniff_utils.py
+++ sniff_utils.py
@@ -2,10 +2,16 @@ from scapy.all import *
 from wifi_utils import *
 from deauth_utils import *
 
+ignore = []
+
+def construct_ignore_list(skip):
+    #check network mac, it may not be initialized.
+    ignore = ["ff:ff:ff:ff:ff:ff", "00:00:00:00:00:00", "33:33:00:", "33:33:ff:", "01:80:c2:00:00:00", "01:00:5e:", network.interface_mac]
+    if skip:
+        ignore += [addr.lower() for addr in skip]
+
 
 def noise_filter(addr1, addr2):
-    ignore = ['ff:ff:ff:ff:ff:ff', '00:00:00:00:00:00', '33:33:00:',
-              '33:33:ff:', '01:80:c2:00:00:00', '01:00:5e:']
     for i in ignore:
         if i in addr1 or i in addr2:
             return True
@@ -13,9 +19,8 @@ def noise_filter(addr1, addr2):
 
 
 def analyze_pkt(pkt):
-    if pkt.haslayer(Dot11):
+    if pkt.haslayer(Dot11FCS):
         if pkt.addr1 and pkt.addr2:
-            print(pkt.addr1)
             pkt.addr1 = pkt.addr1.lower()
             pkt.addr2 = pkt.addr2.lower()
         if noise_filter(pkt.addr1, pkt.addr2):
@@ -24,17 +29,7 @@ def analyze_pkt(pkt):
             print(pkt.addr1)
             # network.add_client(pkt.addr2, pkt.addr1)
 
-
-def PacketHandler(packet):
-    if packet.haslayer(Dot11):
-        if packet.type == 0 and packet.subtype == 8:
-                print("Access Point MAC: %s with SSID: %s " %(packet.addr2, packet.info))
-
-iwlist_scan("wlan0")
-prepare_attack()
-atexit.register(exit_handler)
 try:
-    while (1):
-        sniff(iface="wlan0", store=False, prn = PacketHandler, count=1)
+    sniff(iface="wlan0", store=False, prn = analyze_pkt)
 except Exception as e:
     print(str(e))
